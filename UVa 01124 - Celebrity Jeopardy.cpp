e55ee7132370ad396369e7f1e4fa0201a3817d67
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2020-10-06 22:34:47 +0700

    Create UVa 01124 - Celebrity Jeopardy.cpp

---
+++ UVa 01124 - Celebrity Jeopardy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	char c;
+	while (~(c = getchar()))
+		cout << c;
+	
+	return 0;
+}
