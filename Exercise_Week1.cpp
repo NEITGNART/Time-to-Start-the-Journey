dbd3d87d757ddca654900006e193fe7d780ef778
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2020-10-26 23:34:09 +0700

    Update Exercise_Week1.cpp

--- Exercise_Week1.cpp
+++ Exercise_Week1.cpp
@@ -106,3 +106,140 @@ int main(void) {
 	cout << Factorial(6);
 	return 0;
 }
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+#define vi vector<int>
+
+struct Examinee
+{
+	string id;
+	string name;
+	float math, literature, physic, chemistry, biology, history, geography, civic_education, natural_science,
+		social_science, foreign_language;
+	string note;
+	string provincial;
+};
+
+
+void readExamineeList(vector<Examinee>& list, string name) {
+
+	ifstream fin(name.c_str());
+	if (fin.fail()) return;
+
+	string buffer;
+	getline(fin, buffer);
+	
+		while (fin.good()) {
+		Examinee object;
+
+		string id;
+		getline(fin, id, ','); 
+		if (id.empty()) object.id = "";
+		else object.id = id;
+		cout << id << endl;
+
+
+		string name;
+		getline(fin, name, ',');
+		if (name.empty()) object.name = "";
+		else object.name = name;
+		cout << name << endl;
+			
+		string math;
+		getline(fin, math, ','); 
+		
+		if (math.empty()) object.math = 0;
+		else object.math = stof(math);
+		cout << math << endl;
+
+		string literature;
+		getline(fin, literature, ','); 
+		if (literature.empty()) object.literature = 0;
+		else object.literature = stof(literature);
+
+		cout << literature << endl;
+
+		string physics;
+		getline(fin, physics, ',');
+		if (physics.empty()) object.physic = 0;
+		else object.physic = stof(physics);
+		cout << physics << endl;
+		string chemistry;
+		getline(fin, chemistry, ','); 
+		if (chemistry.empty()) object.chemistry = 0;
+		else object.chemistry= stof(chemistry);
+		cout << chemistry << endl;
+		string bio;
+		getline(fin, bio, ','); 
+		if (bio.empty()) object.biology = 0;
+		else object.biology = stof(bio);
+		cout << bio << endl;
+		string history;
+		getline(fin, history, ','); 
+		if (history.empty()) object.history = 0;
+		else object.history = stof(history);
+		cout << history << endl;
+		string geo;
+		getline(fin, geo, ','); 
+		if (history.empty()) object.geography = 0;
+		else object.geography = stof(geo);
+		cout << geo << endl;
+		string civic_education;
+		getline(fin, civic_education, ','); 
+		if (civic_education.empty()) object.civic_education = 0;
+		else object.civic_education = stof(civic_education);
+		cout << civic_education << endl;
+		string natural;
+		getline(fin, natural, ','); 
+		if (natural.empty()) object.natural_science = 0;
+		else object.natural_science = stof(natural);
+		cout << natural << endl;
+		string social;
+
+		getline(fin, social, ',');
+		if (social.empty()) object.social_science = 0;
+		else object.social_science = stof(social);
+		cout << social << endl;
+
+		string foreign;
+		getline(fin, foreign, ','); 
+		if (foreign.empty()) object.foreign_language = 0;
+		else object.foreign_language = stof(foreign);
+		cout << foreign << endl;
+		string note;
+		getline(fin, note, ','); 
+		if (note.empty()) object.note = "";
+		else object.note = (note);
+		cout << note << endl;
+		string provincial;
+		getline(fin, provincial); 
+		if (provincial.empty()) object.provincial = "";
+		else object.note = (provincial);
+
+		cout << provincial << endl << endl << endl;
+		cout << "size: " << list.size() << endl;
+		list.push_back(object);
+	}
+	
+}
+
+
+
+int main(void) {
+
+	vector<Examinee> list;
+
+	string name = "C:/Users/Administrator/source/repos/For_Fun/For_Fun/data.txt";
+	readExamineeList(list, name);
+
+	cout << list.size();
+	return 0;
+
+}
